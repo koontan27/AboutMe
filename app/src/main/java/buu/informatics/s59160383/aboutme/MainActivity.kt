ca6614843a27b63c27defb3fbe83f49d7ca11e88
koontan27 <koontan27@gmail.com>
2019-08-21 03:36:33 +0700

    Binding 4

--- app/src/main/java/buu/informatics/s59160383/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160383/aboutme/MainActivity.kt
@@ -5,15 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import buu.informatics.s59160383.aboutme.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var  binding: ActivityMainBinding
+    private val myName:MyName = MyName("Wasin")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -24,6 +22,7 @@ class MainActivity : AppCompatActivity() {
             nicknameText.setOnClickListener{
                 updateNickname(it)
             }
+            this.myName = this@MainActivity.myName
         }
 
     }
@@ -43,11 +42,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View){
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             nicknameEdit.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
-
             doneButton.visibility = View.GONE
+            invalidateAll()
 
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
---
+++ app/src/main/java/buu/informatics/s59160383/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package buu.informatics.s59160383.aboutme
+
+data class MyName (var name:String = "", var nickname: String = "")
